c911a576fce2160f11d153df6151a5a636f9813d
gxl2632537 <l>
2020-12-12 11:48:58 +0800

    评价商品

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use App\Events\OrderPaid;
+use App\Events\OrderReviewd;
 use App\Listeners\RegisteredListener;
 use App\Listeners\SendOrderPaidMail;
+use App\Listeners\UpdateProductRating;
 use App\Listeners\UpldateProductSoldCount;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Event;
@@ -27,7 +29,10 @@ class EventServiceProvider extends ServiceProvider
         OrderPaid::class=>[
             UpldateProductSoldCount::class,
             SendOrderPaidMail::class
-        ]
+        ],
+        OrderReviewd::class => [
+            UpdateProductRating::class,
+        ],
     ];
 
     /**
